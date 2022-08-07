773bb5bcecfc3afb942d5363f4a118a04ccf94fe
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-06 23:17:46 -0500

    validate zookeeper test failing. Unsure why. Keep checking

---
+++ __tests__/zookeepers.test.js
@@ -0,0 +1,81 @@
+const fs = require ("fs");
+const {
+    filterByQuery,
+    findById,
+    createNewZookeeper,
+    validateZookeeper,
+} = require('../lib/zookeepers');
+
+const { zookeepers } = require('../data/zookeepers.json');
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+const { start } = require("repl");
+jest.mock("fs");
+
+test("creates a zookeeper object", () => {
+    const zookeeper = createNewZookeeper(
+        {name: "Steve", id: "11"},
+        zookeepers
+    );
+    expect(zookeeper.name).toBe("Steve");
+    expect(zookeeper.id).toBe("11");
+});
+
+test("filters by query", () => {
+    const startingZookeepers = [
+        {
+            id: "12",
+            name: "Bob",
+            age: "50",
+            favoriteAnimal: "giraffe",
+        },
+        {
+            id: "15",
+            name: "Jim",
+            age: "61",
+            favoriteAnimal: "chimpanzee",
+        },
+    ];
+    const updatedZookeepers = filterByQuery({ favoriteAnimal: "giraffe" }, startingZookeepers);
+
+    expect(updatedZookeepers.length).toEqual(1);
+});
+
+test("finds a zookeeper id", () => {
+    const startingZookeepers = [
+        {
+            id: "12",
+            name: "Bob",
+            age: "50",
+            favoriteAnimal: "giraffe",
+        },
+        {
+            id: "15",
+            name: "Jim",
+            age: "61",
+            favoriteAnimal: "chimpanzee",
+        }, 
+    ];
+
+    const result = findById("12", startingZookeepers);
+
+    expect(result.name).toBe("Bob");
+});
+
+test("validate zookeeper", () => {
+    const zookeeper = {
+            id: "15",
+            name: "Jim",
+            age: "61",
+            favoriteAnimal: "chimpanzee",
+        };
+    const invalidZookeeper = {
+            id: "15",
+            name: "Jim",
+            age: "61",
+    };
+    const result = validateZookeeper(zookeeper);
+    const result2 = validateZookeeper(invalidZookeeper);
+
+    expect(result).toBe(true);
+    expect(result2).toBe(false);
+});
